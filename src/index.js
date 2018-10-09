4ad7d13b25da04f21dbcb2ccdbecdabc8a50813b
Nick Levkovich <nicklevkovich@gmail.com>
2018-10-10 00:39:04 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,49 +1,41 @@
 class Sorter {
   constructor() {
     this.array = [];
-    this.compareFunction = (function(a,b){
-      return (a-b)
-    })
+    this.compareFunction = (a,b) => {
+      return (a - b)
+    }
   }
 
   add(element) {
     this.array.push(element);
-    // your implementation
   }
 
   at(index) {
     return this.array[index];
-    // your implementation
   }
 
   get length() {
     return this.array.length;
-    // your implementation
   }
 
   toArray() {
     return this.array;
-    // your implementation
   }
 
   sort(indices) {
-    // your implementation
-    var SortedArray=[];
-    if (indices[1]<indices[0]){
+    const SortedArray = [];
+    if (indices[1] < indices[0]) 
       indices.reverse();
-    }
-    for(var i=0;i<indices.length;i++){
+    for(let i = 0; i < indices.length; i++)
       SortedArray.push(this.array[indices[i]]);
-    }
     SortedArray.sort(this.compareFunction);
-    for (var i=0;i<SortedArray.length;i++){
+    for (let i = 0; i < SortedArray.length; i++){
       this.array.splice(indices[i],1,SortedArray[i])
-    }
+    };
   }
 
   setComparator(compareFunction) {
-    // your implementation
-    this.compareFunction=compareFunction;
+    this.compareFunction = compareFunction;
   }
 }
 
